9eb763eb79fd1bb077ebdb1ee48ddf09fbaa35d9
danielwong0115 <daniellwong442@gmail.com>
2025-10-01 12:42:35 -0500

    Add class data from external API

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,31 @@
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
+import { useJsonQuery } from './utilities/fetch';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+interface Course {
+  term: string;
+  number: string;
+  title: string;
+  meets: string;
+}
+
+interface Schedule {
+  title: string;
+  courses: {[id: string]: Course};
+}
+
 
 const App = () => {
+
+  const [json, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+
+  if (error) return <p>Error loading course data: {`${error}`}</p>;
+  if (isLoading) return <p>Loading courses...</p>;
+  if (!json) return <p>No courses found</p>;
+
+  const schedule = json as Schedule;
 
   return (
     <div className='pl-2'>
